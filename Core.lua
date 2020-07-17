93e0010e739f7a35855ae42baf7d0b4288612f5d
Kyle Buller <bullerk@gmail.com>
2020-07-17 02:47:05 -0500

    Remove pcall
    PlaySoundFile no longer errors for invalid files.

--- Core.lua
+++ Core.lua
@@ -16,7 +16,7 @@ addon.SendMessage = BigWigsLoader.SendMessage
 local path = "Interface\\AddOns\\BigWigs_Voice\\Sounds\\%s.ogg"
 local pathYou = "Interface\\AddOns\\BigWigs_Voice\\Sounds\\%sy.ogg"
 local function handler(event, module, key, sound, isOnMe)
-	local success = pcall(PlaySoundFile, format(isOnMe and pathYou or path, tostring(key)), "Master")
+	local success = PlaySoundFile(format(isOnMe and pathYou or path, tostring(key)), "Master")
 	if not success then
 		addon:SendMessage("BigWigs_Sound", module, key, sound) 
 	end
